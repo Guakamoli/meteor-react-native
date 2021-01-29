3ab4af7b1b7ff905f2d75ea79c312507d8327175
lyihuan <15170674326@163.com>
2021-01-29 10:46:09 +0800

    (feat) delete loginWithSms

--- src/index.js
+++ src/index.js
@@ -8,21 +8,5 @@ const {
     withTracker, Mongo, packageInterface, ReactiveDict
 } = Meteor;
 
-Meteor.loginWithSms = function (phoneNumber, verificationCode, callback) {
-    Accounts.callLoginMethod({
-        methodArguments: [{
-            sms: true,
-            phoneNumber,
-            verificationCode
-        }],
-        userCallback: callback
-    });
-};
-
-
-Meteor.sendVerificationCode = function (phoneNumber, callback) {
-    Meteor.call("accounts-sms.sendVerificationCode", phoneNumber, callback);
-};
-
 export { Accounts, withTracker, Mongo, packageInterface, ReactiveDict };
 export default Meteor;
